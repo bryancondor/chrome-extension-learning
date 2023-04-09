dd30cb793c8cba3ee7e0e5ded124a3dd2db69623
Bryan Condor <bryanalexandercd@gmail.com>
2023-04-09 00:29:17 -0500

    using content-script in extension to add context to existing DOM

---
+++ reading-time/scripts/content.js
@@ -0,0 +1,29 @@
+const article = document.querySelector("article");
+
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (article) {
+    // alert('hello');
+    const text = article.textContent;
+    const wordMatchRegExp = /[^\s]+/g; // Regular expression
+    const words = text.matchAll(wordMatchRegExp);
+    // matchAll returns an iterator, convert to array to get word count
+
+    
+    const wordCount = [...words].length;
+    const readingTime = Math.round(wordCount / 200);
+    const badge = document.createElement("p");
+    // Use the same styling as the publish information in an article's header
+    badge.classList.add("color-secondary-text", "type--caption");
+    badge.textContent = `⏱️ ${readingTime} min de lectura (Hola Mirella)`;
+
+    // Support for API reference docs
+    // const heading = article.querySelector("h1");
+    const heading = document.querySelector("h1");
+    // Support for article docs with date
+    const date = article.querySelector("time")?.parentNode;
+
+    // update content
+    // heading.innerHTML = "Hola soy bryan"
+
+    (date ?? heading).insertAdjacentElement("afterend", badge);
+}
